fe9c5f9c57e5696366672e4b72ba31aa53396f10
Joan-Sebastian-Velandia-Guataquira <joan_velandia@javeriana.edu.co>
2021-03-07 22:32:46 -0500

    client v0.0.0.01

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ClientComponent } from './client.component';
 import { HeaderComponent } from './header/header.component';
 import { ClientRoutingModule } from './client-routing.module';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoutComponent } from './logout/logout.component';
+import { CartComponent } from './cart/cart.component';
+import { OrdersComponent } from './orders/orders.component';
 
 
 
 @NgModule({
-  declarations: [ClientComponent, HeaderComponent],
+  declarations: [ClientComponent, HeaderComponent, MenuComponent, FooterComponent, LogoutComponent, CartComponent, OrdersComponent],
   imports: [
     ClientRoutingModule,
     CommonModule
