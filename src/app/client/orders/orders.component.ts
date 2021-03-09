77988284f5563b8c2e84978fe5ac4e0660617df5
Joan-Sebastian-Velandia-Guataquira <joan_velandia@javeriana.edu.co>
2021-03-08 21:04:45 -0500

    orders work
    Co-authored-by: Jenifer Medina <Jemeye@users.noreply.github.com>

--- src/app/client/orders/orders.component.ts
+++ src/app/client/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/core/models/order/order.model';
 import { User } from 'src/app/core/models/user/user.model';
 import { LocalStorageService } from 'src/app/core/services/localStorage/local-storage.service';
 import { OrdersService } from 'src/app/core/services/orders/orders.service';
@@ -11,14 +12,15 @@ import { OrdersService } from 'src/app/core/services/orders/orders.service';
 export class OrdersComponent implements OnInit {
 
   user!: User;
+  orders: Order[] = [];
   constructor(
     private ordersService: OrdersService,
     private localStorageService: LocalStorageService
   ) { }
 
   ngOnInit(): void {
-    /* this.user = localStorage. */
-
+    this.user = this.localStorageService.getItem('CURRENT_USER') as User;
+    this.orders = this.ordersService.getOrders(this.user);
   }
 
 }
